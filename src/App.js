37479a5fdf71ea83dac38394063b025b1ba3310a
Jessy The <jessythe@Jessys-MacBook-Pro.local>
2021-06-29 20:18:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// 1: Import
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import WebFont from "webfontloader";
+import { GlobalStyles } from "./theme/GlobalStyles";
+import { useTheme } from "./theme/useTheme";
+import * as themes from "./theme/schema.json";
+import ThemeSelector from "./ThemeSelector";
 
-function App() {
+// 2: Create a cotainer
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
+
+const App = () => {
+  // 3: Get the selected theme, font list, etc.
+  const { theme, themeLoaded, getFonts } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+    console.log(theme, "what is theme KONNICHIWA here?");
+  }, [themeLoaded]);
+
+  // 4: Load all the fonts
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts(),
+      },
+    });
+  });
+
+  // 5: Render if the theme is loaded.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {themeLoaded && (
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles />
+          <Container style={{ fontFamily: selectedTheme.font }}>
+            <h1>Theme Builder</h1>
+            <p>
+              This is a theming system with a Theme Switcher and Theme Builder.
+              Do you want to see the source code?{" "}
+              <a href="https://github.com/atapas/theme-builder" target="_blank">
+                Click here.
+              </a>
+            </p>
+            <ThemeSelector setter={setSelectedTheme} />
+          </Container>
+        </ThemeProvider>
+      )}
+    </>
   );
-}
+};
 
 export default App;
